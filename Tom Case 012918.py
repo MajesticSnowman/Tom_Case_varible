a4efe3b8ff553d880e7df228290f205fc7f3a9d5
Thomas Case <112898@district.evit.com>
2018-01-30 09:05:15 -0700

    added files

---
+++ Tom Case 012918.py
@@ -0,0 +1,8 @@
+#Tom Case Variable 01/29/18
+
+name = input("Enter your name: ")
+name = name[0].upper() + name[1:].lower()
+print("-----------")
+print(".::" + name + "::.")
+print("-----------")
+
